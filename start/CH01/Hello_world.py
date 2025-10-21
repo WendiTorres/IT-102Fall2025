76bb6fe5025d4788ad85a782484968f4dbf9cf6f
WendiTorres <Torres.Wendi@student.greenriver.edu>
2025-10-20 19:18:30 -0700

    synchronize

--- start/CH01/Hello_world.py
+++ start/CH01/Hello_world.py
@@ -1,1 +1,19 @@
 print("Hello World")
+
+#Prompts of the user for their name
+name = input("Please enter your name:  ")
+
+postive_affirmation = "This quarter is going to be the best!"
+#Print hello name?
+print(name)
+
+#Print a positive affirmation
+print(postive_affirmation)
+print("This is going to be amazing! ")
+print("chicken nugget chicken nugget ")
+
+#How do we prompt the user for their age?]
+age = int(input("What is your age? "))
+
+#Print out in 2 years you will be xx years old?
+print(age + 2)
