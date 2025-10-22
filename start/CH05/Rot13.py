380b3ef357f1c0758ee2e0e820605e5931bcbcb3
WendiTorres <Torres.Wendi@student.greenriver.edu>
2025-10-22 14:52:17 -0700

    work

--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # Script that encrypts/decrypts text using ROT13
-# By 
+# By Wendi
+
+#Create a simple Caesar Cipher encryption
+
+import string 
+
+plain_text = "Have a nice day!"
+
+shift = 24
+
+
+alphabet = string.ascii_lowercase
+shifted = alphabet[shift:] + alphabet[:shift]
